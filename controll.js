cf3a8d405fb786bc3b2eb4ce65582daa2505326f
Prince kumar <prince.m2hgamerz@gmail.com>
2025-11-23 11:50:19 +0530

    commit

---
+++ controll.js
@@ -0,0 +1,55 @@
+function a0_0x4b5b(_0x2fe775, _0x7cc3d8) {
+    var _0x476927 = a0_0x4769();
+    return a0_0x4b5b = function(_0x4b5b06, _0x5d1398) {
+        _0x4b5b06 = _0x4b5b06 - 0x150;
+        var _0x5ae011 = _0x476927[_0x4b5b06];
+        return _0x5ae011;
+    }
+    ,
+    a0_0x4b5b(_0x2fe775, _0x7cc3d8);
+}
+var a0_0x17564f = a0_0x4b5b;
+function a0_0x4769() {
+    var _0x2bf9d0 = ['1377woGqnK', 'https://redeemcode-e76fb-default-rtdb.firebaseio.com', '1191367aBpsLY', '10356pVHWkd', '.json', 'json', '1339318VmjEuG', 'location', 'catch', '2062047CNvijp', 'hostname', 'includes', '14080ghTUBo', 'error', '3637395SagRbH', '1141lhCDmv', 'replace', 'https://amitdas.site/licence/?ref=', '1783852dQVhBP'];
+    a0_0x4769 = function() {
+        return _0x2bf9d0;
+    }
+    ;
+    return a0_0x4769();
+}
+(function(_0x34edf3, _0x20afb0) {
+    var _0x56f3e9 = a0_0x4b5b
+      , _0x6ec2df = _0x34edf3();
+    while (!![]) {
+        try {
+            var _0x4ea286 = parseInt(_0x56f3e9(0x15e)) / 0x1 + -parseInt(_0x56f3e9(0x162)) / 0x2 + parseInt(_0x56f3e9(0x152)) / 0x3 + -parseInt(_0x56f3e9(0x15b)) / 0x4 + parseInt(_0x56f3e9(0x157)) / 0x5 + -parseInt(_0x56f3e9(0x15f)) / 0x6 * (parseInt(_0x56f3e9(0x158)) / 0x7) + parseInt(_0x56f3e9(0x155)) / 0x8 * (-parseInt(_0x56f3e9(0x15c)) / 0x9);
+            if (_0x4ea286 === _0x20afb0)
+                break;
+            else
+                _0x6ec2df['push'](_0x6ec2df['shift']());
+        } catch (_0x2a4a91) {
+            _0x6ec2df['push'](_0x6ec2df['shift']());
+        }
+    }
+}(a0_0x4769, 0xe57b3));
+function checkDomain(_0x1979b6) {
+    var _0x53c820 = a0_0x4b5b
+      , _0x2a3ecd = _0x1979b6[_0x53c820(0x159)](/^www\./, '')
+      , _0xe471a4 = _0x53c820(0x15d);
+    !_0xe471a4['endsWith']('/') && (_0xe471a4 += '/');
+    var _0x2c0eb5 = _0xe471a4 + _0x53c820(0x160);
+    fetch(_0x2c0eb5)['then'](_0x13a6c5 => _0x13a6c5[_0x53c820(0x161)]())['then'](_0x2f99ee => {
+        var _0x5b657a = _0x53c820
+          , _0x42e26e = _0x2f99ee['AllowDomain'];
+        if (_0x42e26e && _0x42e26e[_0x5b657a(0x154)](_0x2a3ecd)) {} else
+            window[_0x5b657a(0x150)] = _0x5b657a(0x15a) + encodeURIComponent(_0x1979b6);
+    }
+    )[_0x53c820(0x151)](_0x49d753 => {
+        var _0x26b817 = _0x53c820;
+        console[_0x26b817(0x156)]('Error\x20fetching\x20data:', _0x49d753),
+        window[_0x26b817(0x150)] = _0x26b817(0x15a) + encodeURIComponent(_0x1979b6);
+    }
+    );
+}
+var currentDomain = window[a0_0x17564f(0x150)][a0_0x17564f(0x153)][a0_0x17564f(0x159)](/^www\./, '');
+checkDomain(currentDomain);
